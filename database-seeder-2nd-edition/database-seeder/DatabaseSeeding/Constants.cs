ac1f969253b3af2a504756df3104ed24beb9759a
Stacy Cashmore <33732891+StacyCash@users.noreply.github.com>
2024-10-20 14:37:56 +0200

    Update database seeder for 2nd edition
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/StacyCash/beginning-static-web-app-db-seeder?shareId=XXXX-XXXX-XXXX-XXXX).

---
+++ database-seeder-2nd-edition/database-seeder/DatabaseSeeding/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace CosmosDBPlayground.DatabaseSeeding;
+
+internal static class Constants
+{
+    public static string CSharp => "C#";
+    public static string Java => "Java";
+    public static string Angular => "Angular";
+    public static string TypeScript => "TypeScript";
+    public static string Azure => "Azure";
+    public static string Functions => "Functions";
+    public static string StaticWebApps => "Static Web Apps";
+    public static string Blazor => "Blazor";
+}
---
+++ database-seeder-2nd-edition/database-seeder/DatabaseSeeding/GlobalConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace CosmosDBPlayground.DatabaseSeeding;
+
+internal static class GlobalConstants
+{
+    public static string Author => "Freya Christal Garfield";
+}
